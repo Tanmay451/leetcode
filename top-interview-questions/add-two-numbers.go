4ae5b362e48e584304080c330373f069ed6c27bf
tanmay <tanmay.k@gopherslab.com>
2023-02-19 22:40:49 +0530

    added explanation

--- top-interview-questions/add-two-numbers.go
+++ top-interview-questions/add-two-numbers.go
@@ -33,3 +33,16 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return dummyHead.Next
 }
+
+// This is a Go programming language implementation of a function called addTwoNumbers. This function takes two singly linked lists l1 and l2 as input and returns a singly linked list that represents the sum of the two input lists.
+
+// The function works as follows:
+
+// The function initializes a dummy head node dummyHead and a current node pointer curr to the dummy head node. It also initializes a variable carry to zero, which represents the carryover from the previous digit addition.
+// The function enters a loop that continues until both input lists l1 and l2 are empty.
+// For each iteration of the loop, the function extracts the current digit values from each input list by checking the Val field of the current nodes l1 and l2. If either input list has reached the end, the corresponding value is assumed to be zero.
+// The function calculates the sum of the current digit values and the carryover carry. If the sum is greater than or equal to 10, the carryover is set to 1, and the sum modulo 10 is added to the new node's value. Otherwise, the carryover is set to 0, and the sum is added directly to the new node's value.
+// The function creates a new node with the computed value and sets it as the next node in the output list, which is pointed to by curr. The curr pointer is then advanced to the next node.
+// Once both input lists have been fully traversed, the function checks if there is a final carryover that needs to be added to the output list. If there is, it creates a new node with the carryover value and appends it to the output list.
+// The function returns the output list, which is the list of nodes pointed to by the Next field of the dummy head node.
+// This approach effectively performs digit-by-digit addition with carryover, which is a common approach to adding two numbers represented as linked lists. The use of a dummy head node allows for easier management of the output list without having to handle the special case of the head node separately.
--- top-interview-questions/two-sum.go
+++ top-interview-questions/two-sum.go
@@ -11,3 +11,15 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{0, 0}
 }
+
+// This is a Go programming language implementation of a function called twoSum. This function takes an array of integers nums and an integer target as input and returns an array of two integers that represent the indices of the two numbers in the input array that add up to the target.
+
+// Here's how the function works:
+
+// The function initializes a map called m to keep track of the elements in the input array and their indices.
+// The function enters a loop that iterates over each element in the input array.
+// For each element nums[i] in the input array, the function checks if the complement target-nums[i] is already in the m map. The complement is the number that, when added to nums[i], results in the target. The map is used to store the complement number as the key and the index of the corresponding element in the array as the value.
+// If the complement is found in the map, the function returns an array of the indices of the two numbers that add up to the target, which are the value of the complement key in the map and the current index i.
+// If the complement is not found in the map, the current element nums[i] is added to the map with its index i as the value.
+// If the loop completes without finding a complement that adds up to the target, the function returns an array of two zeros.
+// The use of the map m helps to improve the time complexity of the function from O(n^2) to O(n), since checking if an element is in a map takes constant time on average.
